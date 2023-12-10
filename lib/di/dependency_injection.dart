0a82d69d7d162075bd17179fcf485115e14f0e8f
Lxini_G <88278635+LxiniG@users.noreply.github.com>
2023-12-11 00:49:04 +0100

    cleanup

--- lib/di/dependency_injection.dart
+++ lib/di/dependency_injection.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rotteck_messenger/data/database/mongo_db_client.dart';
-import 'package:rotteck_messenger/data/repositories/chat_repository_impl.dart';
-import 'package:rotteck_messenger/data/repositories/user_repository_impl.dart';
-import 'package:rotteck_messenger/domain/repositories/chat_repository.dart';
-import 'package:rotteck_messenger/domain/repositories/user_repository.dart';
 
 final getIt = GetIt.instance;
 
@@ -13,11 +9,4 @@ void setupDependencies() {
 
   final db = MongoDBClient(connectionString: connectionString!);
   db.connect();
-
-  getIt.registerSingleton<MongoDBClient>(db);
-  getIt.registerSingleton<UserRepository>(
-      UserRepositoryImpl(mongoDBClient: getIt<MongoDBClient>()));
-
-  getIt.registerSingleton<ChatRepository>(
-      ChatRepositoryImpl(mongoDBClient: getIt<MongoDBClient>()));
 }
